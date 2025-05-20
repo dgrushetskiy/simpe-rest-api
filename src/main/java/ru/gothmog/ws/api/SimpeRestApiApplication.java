f465997e38762b74e5bd04de32cf56e31d4a7782
dgrushetskiy <dgrushetskiy@gmail.com>
2025-05-20 08:07:41 +0300

    add project

---
+++ src/main/java/ru/gothmog/ws/api/SimpeRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpeRestApiApplication.class, args);
+	}
+
+}
